7f8e601155f491752961ee7585cec7d338f3c44f
S.Penyaz <work.spenyaz@gmail.com>
2023-06-02 13:35:48 +0200

    Added local storage

---
+++ src/repository/Repository.js
@@ -0,0 +1,4 @@
+export const readLocalStorage = () => JSON.parse(localStorage.getItem("tasks"));
+
+export const writeToLocalStorage = (tasks) =>
+  localStorage.setItem("tasks", JSON.stringify(tasks));
