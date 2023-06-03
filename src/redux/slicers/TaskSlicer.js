a14e2527dddf5310b0f640d798213263c8643fcf
S.Penyaz <work.spenyaz@gmail.com>
2023-06-03 15:54:47 +0200

    Added store slice and integrated redux

---
+++ src/redux/slicers/TaskSlicer.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+
+export const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.tasks.push(action.payload);
+    },
+  },
+});
+
+export const {
+  addTask
+} = tasksSlice.actions;
+
+export default tasksSlice.reducer;
