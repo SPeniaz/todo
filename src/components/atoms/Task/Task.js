44732f51a59cdda34b01626cd75d9022f853d579
S.Penyaz <work.spenyaz@gmail.com>
2023-06-21 09:43:51 +0200

    Added ClassNames package for conditional class names

--- src/components/atoms/Task/Task.js
+++ src/components/atoms/Task/Task.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Task.css';
+import classNames from 'classnames';
 
 export function Task({ description, completed }) {
-  return <div className={`Task ${completed ? 'TaskInActive' : ''}`}>{description}</div>;
+  return <div className={classNames('Task', {'TaskInActive' : completed})}>{description}</div>;
 }
 
 export default Task;
--- src/components/atoms/TasksFilterBtn/TaskFilterBtn.js
+++ src/components/atoms/TasksFilterBtn/TaskFilterBtn.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './TaskFilterBtn.css';
+import classNames from 'classnames';
 
 export function TaskFilterBtn({ children, onClick, active }) {
-  const buttonClass = active ? 'TaskFilterBtn Active' : 'TaskFilterBtn';
-
   return (
-    <button type="button" className={buttonClass} onClick={onClick}>
+    <button type="button" className={classNames('TaskFilterBtn', {'Active': active})} onClick={onClick}>
       {children}
     </button>
   );
